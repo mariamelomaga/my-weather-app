ba1c936b4abe6f2d8f944fe086d7ae20ce043949
mariamelomaga <maria.melo.maga@gmail.com>
2021-04-23 14:00:16 +0100

    Update on all code

--- src/index.js
+++ src/index.js
@@ -1,61 +1,101 @@
-let date = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[date.getDay()];
-let hour = date.getHours();
-if (hour < 10) {
-  hour = `0${hours};`;
-}
-let minutes = date.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}; `;
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentTime.getDay()];
+  let hour = currentTime.getHours();
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = currentTime.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hour} : ${minutes}`;
 }
 
+let currentTime = new Date();
 let h2 = document.querySelector("#date");
-h2.innerHTML = `${day} ${hour} : ${minutes}`;
+h2.innerHTML = formatDate(currentTime);
 
-function search(event) {
+function displayWeather(response) {
+  console.log(response.data.name);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+// display by default a city , on load!
+function search(city) {
+  let units = "metric";
+  let apiKey = "5a12dd671f298e4a72d4ec34c2cdc4ee";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+//make an API call
+function handleSubmit(event) {
+  debugger;
   event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = searchInput.value;
+  let city = document.querySelector("#city-input").value;
+  search(city);
 }
 let submit = document.querySelector("#search-form");
-submit.addEventListener("submit", search);
+submit.addEventListener("submit", handleSubmit);
 
-function fahrenheit(event) {
+// units
+function convertCelsius(event) {
   event.preventDefault();
-  let degreesElement = document.querySelector("#temperature");
-  degreesElement.innerHTML = 66;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 22;
+}
+
+function convertFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
 }
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", fahrenheit);
+fahrenheitLink.addEventListener("click", convertFahrenheit);
 
-function celsius(event) {
-  event.preventDefault();
-  let degreesElement = document.querySelector("#temperature");
-  degreesElement.innerHTML = 22;
-}
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", celsius);
+celsiusLink.addEventListener("click", convertCelsius);
 
-function showPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
+//city on load
+search("lisbon");
+
+// current location button - with geolocation API - copy different APIurl from openweather
+
+//3
+function showLocation(position) {
   let units = "metric";
   let apiKey = "5a12dd671f298e4a72d4ec34c2cdc4ee";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
-navigator.geolocation.getCurrentPosition(showPosition);
-
-function showTemperature(response) {}
+//2 we could copy this from web - "js current location"
+function currentWeatherLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+//1
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", currentWeatherLocation);
