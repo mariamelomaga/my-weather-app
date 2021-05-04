a7940e136e6566170fcfb0a3f932454547bb33ad
mariamelomaga <maria.melo.maga@gmail.com>
2021-05-04 19:01:32 +0100

    Add the forecast HTML to JS

--- src/index.js
+++ src/index.js
@@ -25,6 +25,27 @@ function formatDate(timestamp) {
   return `${day} ${hour}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `  <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img src="images/cloudy.png" width="36" />
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max"> 23</span>
+              <span class="weather-forecast-temperature-min"> 12</span>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -101,7 +122,7 @@ let celsiusTemperature = null;
 
 //city on load
 search("lisbon");
-
+displayForecast();
 // current location button - with geolocation API - copy different APIurl from openweather
 
 //3
